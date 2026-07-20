ef0f7c73202bb1a7d15aaaef821b12cdf369055c
Léo Sano <leosano17@gmail.com>
2026-07-20 20:25:08 -0300

    adiciona domínio da Vercel no CORS

--- src/main/java/com/fintech/gestao_financeira/config/SecurityConfig.java
+++ src/main/java/com/fintech/gestao_financeira/config/SecurityConfig.java
@@ -49,7 +49,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://gestao-financeira-frontend-mirbizijq-lsano.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
