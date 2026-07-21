91213422b5b60e27cb715ac9de2344bec010393b
Léo Sano <leosano17@gmail.com>
2026-07-20 21:12:12 -0300

    adiciona forward headers strategy

--- src/main/java/com/fintech/gestao_financeira/GestaoFinanceiraApplication.java
+++ src/main/java/com/fintech/gestao_financeira/GestaoFinanceiraApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GestaoFinanceiraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestaoFinanceiraApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoFinanceiraApplication.class, args);
+    }
 
 }
